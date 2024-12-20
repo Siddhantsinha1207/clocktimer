e1e4de52880d46e7ff9695cebb58b960c3449dbc
Siddhantsinha1207 <sidhantkumarsinha@gmail.com>
2024-12-20 20:10:47 +0530

    logic updates

--- script.js
+++ script.js
@@ -8,19 +8,29 @@ const clearBtn = document.querySelector(".clear");
 console.log(seconds.value);
 
 startBtn.addEventListener("click", function () {
-  if (seconds.value <= 0) return;
+  startBtn.disabled = true;
+  seconds.disabled = true;
+  if (+seconds.value <= 0 && +minutes.value <= 0) return;
   if (seconds.value > 60) {
     minutes.value = Math.floor(seconds.value / 60);
     seconds.value = seconds.value % 60;
   }
   const timer = setInterval(function () {
-    console.log(seconds.textContent);
     seconds.value = seconds.value - 1;
     hours.textContent = hours.textContent.padStart(2, 0);
     minutes.textContent = minutes.textContent.padStart(2, 0);
     seconds.value = seconds.value.padStart(2, 0);
 
-    if (Number(seconds.value) === 0) clearInterval(timer);
+    if (+minutes.value > 0 && +seconds.value === 0) {
+      minutes.value = +minutes.value - 1;
+      seconds.value = 60;
+    }
+
+    if (Number(seconds.value) === 0) {
+      clearInterval(timer);
+      startBtn.disabled = false;
+      seconds.disabled = false;
+    }
   }, 1000);
 });
 
@@ -28,4 +38,5 @@ clearBtn.addEventListener("click", function () {
   seconds.value = seconds.value.padStart(2, 0);
   seconds.value = minutes.value.padStart(2, 0);
   seconds.value = hours.value.padStart(2, 0);
+  return;
 });
